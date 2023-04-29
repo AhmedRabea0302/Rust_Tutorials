0464ab108f016d0d9e6028174aac71668bd64e9b
Ahmed Abo Elfadle <arabea169@gmail.com>
2023-04-29 15:58:00 +0200

    Fixed Unused variables & scope problems

---
+++ functions/target/debug/deps/functions-982cf7976cd19eb8.d
@@ -0,0 +1,5 @@
+/home/fadlovich/Public/rust-tut/functions/target/debug/deps/functions-982cf7976cd19eb8: src/main.rs
+
+/home/fadlovich/Public/rust-tut/functions/target/debug/deps/functions-982cf7976cd19eb8.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/fadlovich/Public/rust-tut/functions/target/debug/functions: /home/fadlovich/Public/rust-tut/functions/src/main.rs
