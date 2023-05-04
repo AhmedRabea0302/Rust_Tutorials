2ba615f1e4012e7bb447c22af6c7ea24e5acd40a
Ahmed Abo Elfadle <arabea169@gmail.com>
2023-05-04 20:49:41 +0200

    Added Ownership and references

---
+++ ownershipAndReferences/target/debug/deps/ownershipAndReferences-d8ed6ab53059b67a.d
@@ -0,0 +1,5 @@
+/home/fadlovich/Public/rust-tut/ownershipAndReferences/target/debug/deps/ownershipAndReferences-d8ed6ab53059b67a: src/main.rs
+
+/home/fadlovich/Public/rust-tut/ownershipAndReferences/target/debug/deps/ownershipAndReferences-d8ed6ab53059b67a.d: src/main.rs
+
+src/main.rs:
---
+++ ownershipAndReferences/target/debug/ownershipAndReferences.d
@@ -0,0 +1,1 @@
+/home/fadlovich/Public/rust-tut/ownershipAndReferences/target/debug/ownershipAndReferences: /home/fadlovich/Public/rust-tut/ownershipAndReferences/src/main.rs
